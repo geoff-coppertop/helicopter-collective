c5c65f91d80aacb9c07386e871beb48fcadd9582
Geoffrey Thomas <geoff.coppertop@gmail.com>
2025-05-10 12:53:52 +0000

    Create blinky example

---
+++ examples/blinky.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![no_main]
+#![feature(impl_trait_in_assoc_type)]
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_rp::gpio;
+use embassy_time::{Duration, Timer};
+use gpio::{Level, Output};
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    info!("Program start");
+    let p = embassy_rp::init(Default::default());
+    let mut led = Output::new(p.PIN_25, Level::Low);
+
+    loop {
+        info!("led on!");
+        led.set_high();
+        Timer::after(Duration::from_millis(100)).await;
+
+        info!("led off!");
+        led.set_low();
+        Timer::after(Duration::from_millis(2500)).await;
+    }
+}
